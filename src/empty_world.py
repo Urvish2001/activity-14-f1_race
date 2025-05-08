2397edcb6e77a49517647b7a78173b961a7bcbec
Urvish <shahurvish2001@gmail.com>
2025-05-08 18:06:30 -0400

    First commit

--- src/empty_world.py
+++ src/empty_world.py
@@ -95,7 +95,7 @@ def pp():
     global prev_error_linear, integral_linear, prev_error_angular, integral_angular, prev_time
     steering = AckermannDrive()
 
-    L = 0.324  
+    L = 0.3241  
     speed = 1.0
     K_dd = 0.3 
     min_ld = 0.7   
